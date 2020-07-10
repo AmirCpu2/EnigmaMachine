8cf04161547d5ca2720b2782303fe0ec5501b73f
Amircpu <email@amircpu.ir>
2020-07-10 19:11:42 +0430

    Init

---
+++ EnigmaMachine.c
@@ -0,0 +1,21 @@
+
+/*****************************************************
+Project : Enigma Machin Rotors controller
+Author  : Amircpu
+Company : Hope Techy
+GitHub : Amircpu2
+Chip type           : ATmega8
+Clock frequency     : 8.000000 MHz
+*****************************************************/
+
+#include <stdio.h>
+#include<mega8.h>
+#define xtal 8000000
+
+int main()
+{
+    //Initilze
+    DDRD = 0xFF;
+
+    return 0;
+}
