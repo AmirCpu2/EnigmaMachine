686d09f1a34325d978373873ba042c487f87db21
Amircpu <email@amircpu.ir>
2020-07-11 02:34:02 +0430

    Rename File

--- EnigmaMachineRotorsController.c
+++ EnigmaMachineRotorsController.c
@@ -1,6 +1,6 @@
 
 /*****************************************************
-Project : Enigma Machin Rotors controller
+Project : Enigma Machin-Rotors controller
 Author  : Amircpu
 Company : Hope Techy
 GitHub : Amircpu2
@@ -12,10 +12,24 @@ Clock frequency     : 8.000000 MHz
 #include<mega8.h>
 #define xtal 8000000
 
+void initialize()
+{
+	DDRA = 0X0F;
+	DDRC = 0X00;
+	DDRD = 0X00;
+
+	PORTA = 0X00;
+	PORTC = 0X00;
+	PORTD = 0XC0;
+}
+
 int main()
 {
-    //Initilze
-    DDRD = 0xFF;
+	
 
-    return 0;
+	//Config init
+	initialize();
+
+	
+    	return 0;
 }
